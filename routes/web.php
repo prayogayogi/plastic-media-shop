d713041c4fc8029555ca50076e922165ce902225
prayogayogi <yogikpiter@gmail.com>
2022-07-22 13:45:05 +0700

    add file balde

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/frontEnd', function () {
-    return view('frontEnd');
-})->middleware(['auth'])->name('frontEnd');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
