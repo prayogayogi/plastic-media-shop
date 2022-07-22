279c786bc1f8bebcfc2cfc932b6ca7813f1c5962
prayogayogi <yogikpiter@gmail.com>
2022-07-22 09:32:20 +0700

    add auth brezze

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/frontEnd';
 
     /**
      * The controller namespace for the application.
