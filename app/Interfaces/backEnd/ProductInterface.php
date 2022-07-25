140b29d1a1bb0fcc2eb6ae2bdbf1f2a7035503a3
prayogayogi <yogikpiter@gmail.com>
2022-07-25 13:34:50 +0700

    add repository patterns

---
+++ app/Interfaces/backEnd/ProductInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Interfaces\backEnd;
+
+interface ProductInterface
+{
+    public function index();
+}
---
+++ app/Repositories/backEnd/ProductRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\backEnd;
+
+use App\Interfaces\backEnd\ProductInterface;
+
+class ProductRepository implements ProductInterface
+{
+    public function index()
+    {
+    }
+}
